817365e6ff0a420ad3ea8ee22fa32e871b9fd0bb
hosmanis <115313404+hosmanis@users.noreply.github.com>
2026-05-22 22:29:53 +0000

    Added new attack animation for the player character, triggered by pressing 's' or 'down'. Also added a new Slug enemy type and placed them in the level.

---
+++ slug.js
@@ -0,0 +1,13 @@
+// Slug class inherits the properties of the enemies class
+import { Enemy } from './enemy.js';
+
+export class Slug extends Enemy {
+    constructor(x, y, patrolLeft, patrolRight, groundGroup) {
+        super(x, y, patrolLeft, patrolRight, groundGroup);
+        this.sprite.addAni('Sprites/Slugani.png', 4, '32x32');
+        this.sprite.anis.Slugani.frameDelay = 8;
+        this.sprite.anis.Slugani.scale.x = 24 / 30;
+        this.sprite.anis.Slugani.scale.y = 24 / 30;
+        this.sprite.changeAni('Slugani');
+    }
+}
